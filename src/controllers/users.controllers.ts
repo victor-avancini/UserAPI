0ac1c6188d9b5298a5ed7ebcb69bda8e771727e8
Victor Avancini <victor.avancini@hotmail.com>
2024-02-24 21:20:23 -0300

    API updated

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
 import { UsersServices } from "../services/users.services";
 
-export class UsersControllers{
+export class UsersControllers {
     createUser(req: Request, res: Response): Response {
         const usersServices = new UsersServices();
 
         const response = usersServices.createUser(req.body.name, req.body.email)
 
-        return res.status(201).json({ user: response, message: "Usuário cadastrado com sucesso."});
+        return res.status(201).json(response);
     }
 }
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,12 +1,11 @@
 import { generateId, usersDatabase } from "../database/database";
-import { IUser } from "../interfaces/users.interfaces";
 
-export class UsersServices{
+export class UsersServices {
     createUser(name: string, email: string) {
-        const newUser: IUser = {id: generateId(), name, email};
+        const newUser = { id: generateId(), name, email };
 
         usersDatabase.push(newUser)
 
-        return { user: newUser, message: "Usuário cadastrado com sucesso."};
-    }   
+        return { user: newUser, message: "Usuário cadastrado com sucesso." };
+    }
 }
